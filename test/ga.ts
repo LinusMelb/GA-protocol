e0e5c3a99e38e29e56cae766e7fb7ee50c27de6b
LinusMelb <penxumelb@gmail.com>
2021-01-01 12:35:02 +1100

    #6 Use self-defined method to construct url and remove urlCat library (#7)
    * use self-defined method to construct url
    
    * Add new testing environment node:9
    
    * updated jest config
    
    * Update .travis.yml
    
    * Update .travis.yml

--- test/ga.ts
+++ test/ga.ts
@@ -4,31 +4,35 @@ const axios = require('axios').default;
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
     // Do something before request is sent
-    console.log(config.url);
+    console.log(`GA url: ${config.url}`);
     return config;
 }, function (error) {
     // Do something with request error
     return Promise.reject(error);
 });
 
+// Sending hits for validation
 describe('GA', () => {
 
-    const ga = new GA('UA-000000000-1');
+    // Property ID for debug purpose
+    const ga = new GA('UA-2202604-2');
 
     it('Sends a pageview to GA', async () => {
-        const response = await ga.pageView({
+        const response: any = await ga.pageView({
             hostname : 'http://localhost',
             pagePath : '/index',
             pageTitle: 'Hello World',
             // Example custom dimensions
             cd1      : 'Android', // e.g. System
             cd2      : 'English', // e.g. Language
-        });
-        expect(true).toBe(response);
+        }, true);
+        expect(response.status).toBe(200);
+        expect(response.status).toBe(200);
+        expect(response.data.hitParsingResult[0].valid).toBe(true)
     });
 
     it('Sends an event to GA', async () => {
-        const response = await ga.event({
+        const response: any = await ga.event({
             hostname     : 'http://localhost',
             pagePath     : '/index',
             pageTitle    : 'Hello World',
@@ -39,8 +43,11 @@ describe('GA', () => {
             // Example custom dimensions
             cd1          : 'Android', // e.g. System
             cd2          : 'English', // e.g. Language
-        });
-        expect(true).toBe(response);
+        }, true);
+        expect(response.status).toBe(200);
+        expect(response.status).toBe(200);
+        expect(response.data.hitParsingResult[0].valid).toBe(true);
+        
     });
 
 });
